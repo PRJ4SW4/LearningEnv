e2c24677e6b6423a5b1aa400dc71c3153fc0b5ad
Lasse Fink <lassefik3@gmail.com>
2025-04-01 10:47:23 +0200

    feat: Modes og input - tilføjet components for de forskellige modes til Polidle - tilføjet og stylet et input felt for Polidle

--- borgertinget-learning-env/src/Pages/Polidle/Polidle.tsx
+++ borgertinget-learning-env/src/Pages/Polidle/Polidle.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Infobox from "../../components/Polidle/Infobox/Infobox";
+import Input from "../../components/Polidle/Input/Input";
 import styles from "./Polidle.module.css";
 
 const Polidle: React.FC = () => {
@@ -8,6 +9,7 @@ const Polidle: React.FC = () => {
       <h1 className={styles.heading}>Polidle</h1>
       <h2 className={styles.subheading}>Velkommen til Dagens Polidle!</h2>
       <p className={styles.paragraph}>Prøv og gætte dagens politiker</p>
+      <Input />
       <Infobox /> {/* Brug Infobox-komponenten */}
       {/* ... (Inputfelter til brugerens gæt og liste over tidligere gæt) */}
     </div>
---
+++ borgertinget-learning-env/src/components/Polidle/Input/Input.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Input.css";
+
+const Input: React.FC = () => {
+  return (
+    <input type="text" className="input-field" placeholder="Enter text here" />
+  );
+};
+
+export default Input;
